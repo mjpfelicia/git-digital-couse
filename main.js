55c43e0ccfa177c4da8de16f0147fb3ac6b51a1e
bowei <mjpfelicia@gmail.com>
2023-05-28 10:59:22 -0300

    add conteudo

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function funcao(array) {
+    let acc = 0;
+  
+    for (let elemento of array) {
+      acc += elemento;
+    }
+  
+    return (acc / array.length);
+  }
+  
+  console.log(funcao([4, 6, 4, 2]));
+
+let array = [];
+
+
+ array.push(1,2)
